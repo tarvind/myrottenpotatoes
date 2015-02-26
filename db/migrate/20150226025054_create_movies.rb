648cb7b4b16111e93d4c9cfa74b847234856b395
saasbook <saasbook@saasbook.(none)>
2015-02-25 22:53:15 -0400

    edited the create_movies migration

--- db/migrate/20150226025054_create_movies.rb
+++ db/migrate/20150226025054_create_movies.rb
@@ -1,7 +1,15 @@
 class CreateMovies < ActiveRecord::Migration
   def up
-  end
+		create table 'movies' do |t|
+			t.string 'title'
+			t.string 'rating'
+			t.text 'description'
+			t.datetime 'release_date'
+			t.timestamps
+	end
+ end
 
   def down
+		drop_table 'movies'
   end
 end
